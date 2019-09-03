2a1edd4d62e2690ad0fdc7fee370b561bd0a4a65
Juha Itkonen <itkonen.juha@gmail.com>
2019-09-03 22:50:00 +0300

    Lisää Twitter-metatagit hienompia twitterjakoja varten

--- _build.sh
+++ _build.sh
@@ -9,3 +9,4 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 Rscript -e "bookdown::calibre('_book/_main.epub', 'mobi')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+cp cover-image.png _book/
